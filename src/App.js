035484fd972bfb1679d6f6e765f1bb439822cd9d
vishal035 <kumarvishal880288@gmail.com>
2022-08-29 21:18:38 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Main/Card';
+import Button from './components/Button/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="absolute top-0 left-0 w-screen h-screen flex flex-col justify-center items-center overflow-hidden bg-black">
+      <Card className="relative flex flex-col w-96 h-24 justify-end bg-primary-black text-primary text-4xl font-semibold">
+        <Card className="absolute top-0 right-0 w-fit py-2 px-4 ">123</Card>
+        <Card className="absolute right-0 py-2 px-4 w-fit font-medium text-2xl">
+          100 + 123
+        </Card>
+      </Card>
+      <Card className=" relative grid grid-cols-4 w-96 h-[520px] bg-primary-black text-primary-white text-4xl font-normal">
+        <Button className="text-primary">AC</Button>
+        <Button>⨦</Button>
+        <Button>%</Button>
+        <Button className="text-primary bg-slate-800">⨬</Button>
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
+        <Button className="text-primary bg-slate-800">x</Button>
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button className="text-primary bg-slate-800">-</Button>
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button className="text-primary bg-slate-800">+</Button>
+        <Button>0</Button>
+        <Button>.</Button>
+        <Button className="text-primary bg-slate-800 col-span-2 py-2 px-4">
+          =
+        </Button>
+        {/* <Button className="text-primary bg-slate-800">=</Button> */}
+      </Card>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F4AB41',
+        'primary-black': '#555A60',
+        'primary-white': '#E3E9EC',
+      },
+    },
+  },
+  plugins: [],
+};
